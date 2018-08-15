0f712c40ca98b02627a563b797e8083c9e828bc1
GRAYSON_LAPTOP\Grayson <grayson.ingram@att.net>
2018-08-15 08:30:55 -0500

    Created a simple oop program with comments to show that I understand the concepts of polymorphism, abstraction, inheritance, & encapsulation

---
+++ OOP/Animal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Animal
+    {
+        //this is an example of abstraction due to the fact that there can be any number
+        //of animals with a different amount of legs, different name, and different species
+        public int numberofLegs;
+        public string species;
+        public string Name;
+
+        public void SetNumberofLegs(int legs)
+        {
+            numberofLegs = legs;
+        }
+        public void SetSpecies(string Species)
+        {
+            if (String.IsNullOrWhiteSpace(Species))
+            {
+                species = "animal";
+            }
+            species = Species;
+        }
+        public int GetNumberOfLegs()
+        {
+            return numberofLegs;
+        }
+        public string GetSpecies()
+        {
+            return species;
+        }
+        public void SetName(string name)
+        {
+            if (String.IsNullOrWhiteSpace(name))
+            {
+                Name = "Bob";
+            }
+            Name = name;
+        }
+        public string GetName()
+        {
+            return Name;
+        }
+        
+
+    }
+}
---
+++ OOP/Dog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    //example of inheritance with the dog inheriting from animal
+    class Dog : Animal
+    {
+        //This is an example of polymophism in that either method can be called
+        //but one will assign a name to the dog and the other will not
+        public Dog()
+        {
+            SetSpecies("Mammal");
+        }
+        public Dog(string name)
+        {
+            SetSpecies("Mammal");
+            SetName(name);
+        }
+        public new void SetNumberofLegs(int legs)
+        {
+            if (legs > 5)
+            {
+                numberofLegs = 4;
+            }
+            numberofLegs = legs;
+        }
+        //this is an example of encapsulation in that the getmessage is encapsulated from outside use
+        public string Bark()
+        {
+            return getMessage();
+        }
+        private string getMessage()
+        {
+            string name = GetName();
+            int legs = GetNumberOfLegs();
+            string species = GetSpecies();
+            return "My name is " + name + " I have " + legs + " many legs and I am " + species + "!";
+        }
+    }
+}
